31422457134dcb15ab1be0650476531e55f0ffe8
Boubacar Camara <bakarsadio@gmail.com>
2018-10-06 10:51:31 +0200

    Fix and clean but the list of courses contains duplicates

--- src/main/scala/scraper.scala
+++ src/main/scala/scraper.scala
@@ -9,41 +9,56 @@ object Main{
 
   val browser = JsoupBrowser()
 
+  //Page containing a form where the user chooses the academic year, section and cycle
+  val courses_url = ("http://isa.epfl.ch/imoniteur_ISAP/!GEDPUBLICREPORTS.filter?ww_b_list=1&ww_i_reportmodel=1715636965&ww_c_langue=&ww_i_reportModelXsl=1715637059")
+
   def main(args: Array[String]): Unit = {
 
-    //Page containing a form where the user chooses the academic year, section and cycle
-    val url = ("http://isa.epfl.ch/imoniteur_ISAP/!GEDPUBLICREPORTS.filter?ww_b_list=1&ww_i_reportmodel=1715636965&ww_c_langue=&ww_i_reportModelXsl=1715637059")
-
-    val html = browser.get(url)
-
-    val distinctChildren = (html >> elementList("select")).map(_.children)
-    val years :: section :: cycle :: Nil  = distinctChildren.map(_.toList.map(_ >> allText("option")))
-
-    val courses = for (y <- List("2018-2019"); s <- section.drop(20); c <- cycle if (c.contains("Semestre") || s.contains("Mobilité")))   yield findCourses(y, s, c)
-
-    //Write courses in the file
+    val courses = allCourses(browser)
     val file = new File("courses.txt")
     val bw = new BufferedWriter(new FileWriter(file))
     courses.foreach(x => bw.write(x + "\n"))
     bw.close()
   }
 
+  //Returns the list of all courses
+  def allCourses(browser : net.ruippeixotog.scalascraper.browser.Browser): List[String] = {
 
-  def findCourses(y : String, s : String, c : String): List[String] = {
-    val html = browser.get(constructUrl(y,s,c))
-    (html >> elementList(".ww_x_ITEMPLAN")).map(_ >> allText)
+    val courses_form = browser.get(courses_url)
+
+    //Option tags grouped by select
+    val options = (courses_form >> elementList("select")).map(_.children)
+    val years :: section :: cycle :: Nil  = options.map(_.toList.map(x => (x >> attr("value") , x >> allText("option"))))
+    val courses = for (y <- List(years.last); s <- section; c <- cycle if (c._2.contains("Semestre") && !s._2.contains("Mobilité") && !s._2.contains("Passerelle")))
+                  yield findCourses(y, s, c)
+    courses.flatten
+
+
 
   }
 
-  def constructUrl(year : String, section : String, cycle : String): String = (
-    "http://isa.epfl.ch/imoniteur_ISAP/!GEDPUBLICREPORTS.filter?ww_b_list=1&ww_i_reportmodel=1715636965&ww_c_langue=&ww_i_reportModelXsl=1715637059&"
-    + "zz_x_PERIODE_ACAD=" + year + "&ww_x_PERIODE_ACAD=1866893861&"
-    + "zz_x_SECTION="+ section
-    + "&ww_x_SECTION=942293&"
-    + "zz_x_BLOC="
-    + cycle
-    + "&ww_x_BLOC=6683117&dummy=ok"
-    )
+  //Returns the list of courses given the specified parameters
+  def findCourses(year : (String, String), section : (String, String), cycle: (String, String)): List[String] = {
+    val params = Map("ww_x_PERIODE_ACAD" -> year._1,
+                     "zz_x_PERIODE_ACAD" -> year._2,
+                     "ww_x_SECTION" -> section._1,
+                     "zz_x_SECTION" -> section._2 ,
+                     "ww_x_BLOC" -> cycle._1,
+                      "zz_x_BLOC"  -> cycle._2 )
+    val withCourses = browser.get(constructUrl(courses_url,params, "&dummy=ok"))
+    (withCourses >> elementList(".ww_x_ITEMPLAN")).map(_ >> allText)
 
-  def elementToText(e : net.ruippeixotog.scalascraper.model.Element) : String = e >> allText("a")
+  }
+
+  //Constructs and returns the URL by adding parameters to the URL
+  def constructUrl(url : String, params: Map[String, String], additional: String = ""): String = {
+    var target : StringBuilder = new StringBuilder("http://isa.epfl.ch/imoniteur_ISAP/!GEDPUBLICREPORTS.filter?ww_b_list=1&ww_i_reportmodel=1715636965&ww_c_langue=&ww_i_reportModelXsl=1715637059&")
+    params.foreach{case(k ,v) => target.append("&" + k + "=" + v)}
+    target.append(additional)
+    target.toString()
+  }
+
+
+
+
 }
